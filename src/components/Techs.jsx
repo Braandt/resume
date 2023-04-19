0739d3ea552f5c18b58cfed88997cfe238dc3308
Braandt <leoobrandt@hotmail.com>
2023-04-19 12:33:51 -0300

    shadow atenuation

--- src/components/Techs.jsx
+++ src/components/Techs.jsx
@@ -33,7 +33,7 @@ export default function Techs() {
 
 function Tech({ children }) {
     return (
-        <div className="bg-white shadow-md px-4 py-2 rounded-full text-primary-600">
+        <div className="bg-white shadow-sm shadow-black/5  px-4 py-2 rounded-full text-primary-600">
             {children}
         </div>
     )
